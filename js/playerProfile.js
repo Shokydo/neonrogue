123eecb0f99e6ca905dcdf38a6b68247881055c5
zero.xuky <58152941+Shokydo@users.noreply.github.com>
2026-07-18 00:29:51 +0300

    update

---
+++ js/playerProfile.js
@@ -0,0 +1,9 @@
+const defaultCosmetics = {
+    baseColor: '#ffffff',      // Текущий цвет игрока (в лобби белый, в игре меняется классом/настройкой)
+    showArms: false,            // Флаг отрисовки рук (для лобби = false, для игры = true)
+    accessories: [],            // Массив для будущей системы аксессуаров
+    bodySize: 24,
+    headSize: 12
+};
+
+export default defaultCosmetics;
